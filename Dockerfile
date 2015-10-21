64f32cc088d6d27a1a3155480e27ff79ba30ed0d
Tim Lucas <t@toolmantim.com>
2015-10-21 15:24:43 +1100

    Cleanups and switch to standard Ubuntu buildkite-agent package

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM gliderlabs/alpine:3.1
 MAINTAINER Tim Lucas <tim@buildkite.com>
 
 RUN apk --update add curl wget bash git perl openssh-client && \
-    curl -L https://github.com/docker/compose/releases/download/1.4.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && \
-    chmod +x /usr/local/bin/docker-compose && \
-    DESTINATION=/buildkite bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+    curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && \
+    chmod +x /usr/local/bin/docker-compose
+
+RUN DESTINATION=/buildkite bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 
 ENV PATH=$PATH:/buildkite/bin \
     BUILDKITE_BOOTSTRAP_SCRIPT_PATH=/buildkite/bootstrap.sh \
---
+++ test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Builds and tests all the Dockerfiles in this repo to ensure that everything builds AOK
+
+set -eu
+
+# Returns a list like so:
+# ./Dockerfile
+# ./beta/Dockerfile
+# ./dind/1.6.2/Dockerfile
+docker_files=$(find . -name Dockerfile -type f)
+
+for docker_file in $docker_files; do
+  # We want:
+  # ./Dockerfile            -> buildkite-agent:test
+  # ./beta/Dockerfile       -> buildkite-agent:test-beta
+  # ./dind/1.6.2/Dockerfile -> buildkite-agent:test-dind-1.6.2
+
+  # Strip off tailing /Dockerfile
+  tag="${docker_file//\/Dockerfile}"
+  # Strip off leading ./
+  tag="${tag//.\/}"
+  # Replace all / with -
+  tag="${tag//\//-}"
+
+  tag="buildkite-agent-test:$tag"
+
+  # Change into the directory
+  pushd $(dirname "$docker_file") > /dev/null
+
+  echo -e "\033[33;36m--- Building $docker_file as $tag\033[0m"
+  docker build --tag "$tag" .
+
+  echo -e "\033[33;36m--- Testing $tag\033[0m"
+  docker run -it --rm=true --privileged=true "$tag" --version
+
+  echo -e "\033[33;32mLooks good! \\m/\\m/\033[0m"
+
+  popd > /dev/null
+done
+
+echo -e "\033[33;32mAll images looks good!\033[0m"
