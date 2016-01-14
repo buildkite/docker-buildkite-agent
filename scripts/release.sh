4c4528299bbc1cbe47dd9f64b42e4ccb25469226
Lachlan Donald <lachlan@ljd.cc>
2016-01-14 11:06:59 +1100

    Reference tag, not image

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 scripts/list.sh | while read line ; do
   tokens=($line)
   image=${tokens[0]}
-  echo "--- Pushing buildkite/$image"
-  echo docker push "buildkite/$image"
+  echo "--- Pushing buildkite/agent:$image"
+  echo docker push "buildkite/agent:$image"
 done
 
 echo -e "\033[33;32m--- All images released!\033[0m"
--- scripts/test_all_images.sh
+++ scripts/test_all_images.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 scripts/list.sh | while read line ; do
   tokens=($line)
   image=${tokens[0]}
-  ./scripts/test_image.sh "buildkite/$image"
+  ./scripts/test_image.sh "buildkite/agent:$image"
 done
 
 echo -e "\033[33;32m--- All images pass tests!\033[0m"
