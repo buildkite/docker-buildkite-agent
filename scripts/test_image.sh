aec98c08ffab4aca316320689f7b8398801ee28e
Lachlan Donald <lachlan@ljd.cc>
2016-01-14 13:25:30 +1100

    Add an integration test to be sure agent can connect

--- scripts/test_image.sh
+++ scripts/test_image.sh
@@ -14,7 +14,13 @@ docker_label() {
   docker inspect --format "{{ index .Config.Labels \"$label\" }}" "$image"
 }
 
+query_bk_agent_api() {
+  curl --silent -f -H "Authorization: Bearer $BUILDKITE_DOCKER_INTEGRATION_TEST_API_TOKEN" \
+    "https://api.buildkite.com/v1/organizations/$BUILDKITE_DOCKER_INTEGRATION_TEST_ORG_SLUG/agents$@"
+}
+
 DOCKER_IMAGE_NAME="$1"
+BUILD_ID=${BUILDKITE_BUILD_ID:-dev$$}
 
 echo "--- :hammer: Testing ${DOCKER_IMAGE_NAME}"
 
@@ -34,6 +40,26 @@ fi
 if [[ $(docker_label $DOCKER_IMAGE_NAME "com.buildkite.docker_dind") == "true" ]] ; then
   echo "--- :hammer: Checking docker-in-docker for ${DOCKER_IMAGE_NAME}"
   docker run --rm -e DIND=true --privileged --entrypoint "entrypoint.sh" "${DOCKER_IMAGE_NAME}" docker info
+fi
+
+echo "--- :hammer: Checking buildkite agent phones home"
+cidfile="${DOCKER_IMAGE_NAME#*:}.cid"
+trap "docker rm -fv \$(cat $cidfile) >/dev/null; rm $cidfile" EXIT
+
+docker run -d --cidfile $cidfile ${DOCKER_IMAGE_NAME} \
+  start --name "$BUILD_ID" --token "$BUILDKITE_DOCKER_INTEGRATION_TEST_AGENT_TOKEN" >/dev/null
+
+for run in {1..10} ; do
+  sleep 1
+  if query_bk_agent_api "?name=$BUILD_ID" > /dev/null ; then
+    break
+  fi
+done
+
+if ! query_bk_agent_api "?name=$BUILD_ID" | grep -C 20 --color=always '"connection_state": "connected"' ; then
+  echo "Agent didn't connect to buildkite, showing container logs"
+  docker logs $(cat $cidfile)
+  exit 1
 fi
 
 echo -e "\033[33;32mLooks good!\033[0m"
