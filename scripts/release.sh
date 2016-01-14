b775dc74f7f79b36afe57cefe81ca310e90373f9
Lachlan Donald <lachlan@ljd.cc>
2016-01-14 12:16:36 +1100

    Release only runs on master and actually does stuff

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,13 +8,12 @@ scripts/list.sh | while read line ; do
   extratags=${tokens[@]:5}
 
   echo "--- Pushing buildkite/agent:$image"
-  echo docker push "buildkite/agent:$image"
+  docker push "buildkite/agent:$image"
 
   for aliastag in ${extratags[@]} ; do
     echo "--- Pushing buildkite/agent:$aliastag"
-    echo docker push "buildkite/agent:$aliastag"
+    docker push "buildkite/agent:$aliastag"
   done
-
 done
 
 echo -e "\033[33;32m--- All images released!\033[0m"
