f717327ac85ae75de4014443b9c4b15bac201d2f
Lachlan Donald <lachlan@ljd.cc>
2017-01-28 14:32:31 +1100

    Test docker socket

--- .buildkite/steps/test.sh
+++ .buildkite/steps/test.sh
@@ -7,6 +7,9 @@ for tag in ${tags[*]} ; do
   echo "--- :docker: Building buildkite-agent:${tag}"
   docker build --tag "buildkite-agent:${tag}" -f "${tag}/Dockerfile" .
 
-  echo "--- :hammer: Testing buildkite-agent:${tag}"
+  echo "--- :hammer: Testing buildkite-agent:${tag} can run"
   docker run --rm --entrypoint "buildkite-agent" "buildkite-agent:${tag}" --version
+
+  echo "--- :hammer: Testing buildkite-agent:${tag} can access docker socket"
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --entrypoint "docker" "buildkite-agent:${tag}" test
 done
