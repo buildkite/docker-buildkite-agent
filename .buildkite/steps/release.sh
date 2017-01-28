8b20e070401a6b23c3fa51fa244c65537405c64c
Lachlan Donald <lachlan@ljd.cc>
2017-01-28 14:15:01 +1100

    Add a basic release script

--- .buildkite/steps/release.sh
+++ .buildkite/steps/release.sh
@@ -1,4 +1,25 @@
-#!/bin
+#!/bin/bash
+set -euo pipefail
 
-echo "to be implemented"
-exit 1
+tags=(beta edge stable)
+
+for tag in ${tags[*]} ; do
+  echo "--- :docker: Building buildkite-agent:${tag}"
+  docker build --tag "buildkite-agent:${tag}" -f "${tag}/Dockerfile" .
+done
+
+docker push buildkite-agent:stable
+docker push buildkite-agent:beta
+docker push buildkite-agent:edge
+
+# variants of stable - 2.3.2
+for tag in latest 2.3.2 2.3 2 ; do
+  docker tag buildkite-agent:stable "buildkite-agent:$tag"
+  docker push "buildkite-agent:$tag"
+done
+
+# variants of beta 3.0-beta.9
+for tag in 3.0-beta.9 3.0 3 ; do
+  docker tag buildkite-agent:beta "buildkite-agent:$tag"
+  docker push "buildkite-agent:$tag"
+done
