41247fc64ba995586275d9f47e6a5a0359248295
Lachlan Donald <lachlan@ljd.cc>
2017-02-09 19:57:18 +1100

    Add a dedicated compose wrapper

--- .buildkite/steps/release.sh
+++ .buildkite/steps/release.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-DOCKER_IMAGE=${DOCKER_IMAGE:-buildkite/agent}
+: ${DOCKER_IMAGE:=buildkite/agent}
+: ${DOCKER_PUSH:=1}
 
 # parse a docker version like "buildkite-agent version 2.3.2, build 1408"
 # return a series of numbers like [ 2.3.2 2.3 2 ]
@@ -23,22 +24,22 @@ for tag in ${tags[*]} ; do
   docker build --tag "${DOCKER_IMAGE}:${tag}" -f "${tag}/Dockerfile" .
 done
 
-docker images
-
-docker push "${DOCKER_IMAGE}:stable"
-docker push "${DOCKER_IMAGE}:beta"
-docker push "${DOCKER_IMAGE}:edge"
+if [[ "$DOCKER_PUSH" =~ (true|1) ]]; then
+  docker push "${DOCKER_IMAGE}:stable"
+  docker push "${DOCKER_IMAGE}:beta"
+  docker push "${DOCKER_IMAGE}:edge"
+fi
 
 # variants of stable - e.g 2.3.2
 for tag in latest $(parse_version "$(docker run "${DOCKER_IMAGE}:stable" --version)") ; do
-  echo "--- :docker: Tagging and pushing ${DOCKER_IMAGE}:${tag} (stable)"
+  echo "--- :docker: Tagging ${DOCKER_IMAGE}:${tag} (stable)"
   docker tag "${DOCKER_IMAGE}:stable" "${DOCKER_IMAGE}:$tag"
-  docker push "${DOCKER_IMAGE}:$tag"
+  [[ "$DOCKER_PUSH" =~ (true|1) ]] && docker push "${DOCKER_IMAGE}:$tag"
 done
 
 # variants of beta - e.g 3.0-beta.16
 for tag in $(parse_version "$(docker run "${DOCKER_IMAGE}:beta" --version)") ; do
-  echo "--- :docker: Tagging and pushing ${DOCKER_IMAGE}:${tag} (beta)"
+  echo "--- :docker: Tagging ${DOCKER_IMAGE}:${tag} (beta)"
   docker tag "${DOCKER_IMAGE}:beta" "${DOCKER_IMAGE}:$tag"
-  docker push "${DOCKER_IMAGE}:$tag"
+  [[ "$DOCKER_PUSH" =~ (true|1) ]] && docker push "${DOCKER_IMAGE}:$tag"
 done
