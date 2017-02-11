32b2d997da431547de4cbcc0e660f6d2b1f3f355
Lachlan Donald <lachlan@ljd.cc>
2017-02-11 13:55:36 +1100

    Install docker-compose via pip

--- .buildkite/steps/test.sh
+++ .buildkite/steps/test.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
+: ${DOCKER_IMAGE:=buildkite/agent}
+
 tags=(beta edge stable)
 
 for tag in ${tags[*]} ; do
-  echo "--- :docker: Building buildkite-agent:${tag}"
-  docker build --tag "buildkite-agent:${tag}" -f "${tag}/Dockerfile" .
+  echo "--- :docker: Building ${DOCKER_IMAGE}:${tag}"
+  docker build --tag "${DOCKER_IMAGE}:${tag}" -f "${tag}/Dockerfile" .
 
-  echo "--- :hammer: Testing buildkite-agent:${tag} can run"
-  docker run --rm --entrypoint "buildkite-agent" "buildkite-agent:${tag}" --version
+  echo "--- :hammer: Testing ${DOCKER_IMAGE}:${tag} can run"
+  docker run --rm --entrypoint "buildkite-agent" "${DOCKER_IMAGE}:${tag}" --version
 
-  echo "--- :hammer: Testing buildkite-agent:${tag} can access docker socket"
-  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --entrypoint "docker" "buildkite-agent:${tag}" version
+  echo "--- :hammer: Testing ${DOCKER_IMAGE}:${tag} can access docker socket"
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock --entrypoint "docker" "${DOCKER_IMAGE}:${tag}" version
+
+  echo "--- :hammer: Testing ${DOCKER_IMAGE}:${tag} has docker-compose"
+  docker run --rm --entrypoint "docker-compose" "${DOCKER_IMAGE}:${tag}" --version
 done
