017d6cbb998996c7df8df2e68514eeada8d30a75
Lachlan Donald <lachlan@ljd.cc>
2017-02-15 10:45:12 +1100

    Add support for /docker-entrypoint.d/*

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+DIR=/docker-entrypoint.d
+
+if [[ -d "$DIR" ]] ; then
+  /bin/run-parts --verbose "$DIR"
+fi
+
 exec /sbin/tini -g -- ssh-env-config.sh /usr/local/bin/buildkite-agent "$@"
